193cea5f4e00d9a2b0095b2564fb6fdae97b2964
Al-Shimaa Gamal <al-sh.gamal@outlook.com>
2023-08-10 21:48:13 +0300

    flask

--- app.py
+++ app.py
@@ -1,27 +1,45 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
+# Define the upload folder and allowed extensions
 UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+# Function to check if a file has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(request.url)
-    if file:
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filename)
-        # Add image processing and model logic here
-        return redirect(url_for('home'))
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Check if the post request has a file part
+        if 'image' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['image']
+        
+        # If the user does not select a file, the browser submits an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        if file and allowed_file(file.filename):
+            # Save the uploaded file to the upload folder
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            
+            # Perform processing on the uploaded image here (example: copying the image as output)
+            output_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'output_' + file.filename)
+            os.rename(filename, output_filename)
+            
+            return render_template('home.html', output_image=output_filename)
+    
+    return render_template('home.html', output_image=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
