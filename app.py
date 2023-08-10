2263905f0bbf95d4c27439a86108744021eb86fe
Al-Shimaa Gamal <al-sh.gamal@outlook.com>
2023-08-11 02:37:53 +0300

    flask

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, flash, redirect  # Import jsonify
+from functions import process_image
+
 import os
 
 app = Flask(__name__)
@@ -12,34 +14,38 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Function to check if a file has an allowed extension
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # Check if the post request has a file part
         if 'image' not in request.files:
             flash('No file part')
             return redirect(request.url)
         
         file = request.files['image']
         
-        # If the user does not select a file, the browser submits an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         
         if file and allowed_file(file.filename):
-            # Save the uploaded file to the upload folder
             filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(filename)
             
-            # Perform processing on the uploaded image here (example: copying the image as output)
-            output_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'output_' + file.filename)
-            os.rename(filename, output_filename)
+            output_filename = process_image(file.filename)
             
             return render_template('home.html', output_image=output_filename)
     
     return render_template('home.html', output_image=None)
+
+@app.route('/process_image/<input_filename>')
+def process_image_route(input_filename):
+    output_filename = process_image(input_filename)
+    result_info = "Result information goes here..."
+    
+    # Assuming UPLOAD_FOLDER is '/data/nih_new/images-small'
+    output_image_url = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
+    
+    return jsonify({'output_image': output_image_url, 'result_info': result_info})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,40 @@
+import os
+import keras
+from keras import backend as K
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import cv2
+import sklearn
+import lifelines
+import shap
+
+from util import *
+from public_tests import *
+
+
+
+# This sets a common size for all the figures we will draw.
+#plt.rcParams['figure.figsize'] = [10, 7]
+
+path = "./"
+model = load_C3M3_model(path)
+
+IMAGE_DIR = 'data/nih_new/images-small/'
+df = pd.read_csv('data/nih_new/train-small.csv')
+im_path = IMAGE_DIR + '00025288_001.png' 
+x = load_image(im_path, df, preprocess=False)
+# plt.imshow(x, cmap = 'gray')
+# plt.show()
+
+# functions.py
+
+# functions.py
+
+def process_image(input_filename):
+    # Construct the full path to the input image in the dataset directory
+    input_image_path = os.path.join(IMAGE_DIR, input_filename)
+    
+    # Simulated processing logic: Just returning the input image path
+    return input_image_path
+
